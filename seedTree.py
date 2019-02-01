280aabe606d7da80ed19b619da8e8a9d30cd6573
Jason Siil <jason.a.siil@gmail.com>
2019-02-01 15:50:09 -0600

    Made the network directional as well as more Family Tree shaped

--- seedTree.py
+++ seedTree.py
@@ -2,19 +2,22 @@ import networkx as nx
 import matplotlib.pyplot as plt 
 import pandas as pd 
 
-df = pd.read_excel("SeedTest.xlsx")
-df["CrystalName"] = df.apply(lambda row: row.FurnaceID + "-" + str(row.Run), axis= 1)
+df = pd.read_excel("PIN Yield Improvement 01302019.xlsx", sheet_name = "Run Matrix")
+#df["CrystalName"] = df.apply(lambda row: row.FurnaceID + "-" + str(row.Run), axis= 1)
 
+df = df[1:]
+df = df.sort_values("Xtl ID")
+g = nx.from_pandas_edgelist(df,target="Xtl ID", source = "Seed",create_using = nx.DiGraph)
 
-
-g = nx.from_pandas_edgelist(df,"CrystalName", "Seed Crystal")
-
-df = df.set_index("CrystalName")
+df = df.set_index("Xtl ID")
 nodeAtts = []
+
+
+
 
 for node in g.nodes():
     try:
-        Yield = df["Yield"].loc[node]
+        Yield = df["Result"].loc[node]
     except:
         Yield = "Single"
 
@@ -31,5 +34,6 @@ for node in g.nodes():
 g.add_nodes_from(nodeAtts)
 
 colorlist = list(nx.get_node_attributes(g,"Yield").values())
-nx.draw_kamada_kawai(g, with_labels = True,node_color= colorlist, font_weight = 'bold')
+pos = nx.drawing.nx_agraph.graphviz_layout(g, prog="dot")
+nx.draw(g,pos, arrows=True, with_labels = True,node_color= colorlist, font_weight = 'bold',)
 plt.show()
